84277fad38dd39afc5c46fa322f3548a41806c52
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-06-22 19:16:30 +0300

    styled

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtGui import QPalette
+from PyQt5.QtCore import Qt
 import sys
 
 app = QApplication(sys.argv)
+app.setStyle('Fabion')
 print(app)
+
+button = QPushButton('Click')
+def on_button_clicked():
+    alert = QMessageBox()
+    alert.setText('You clicked the button!')
+    alert.exec_()
+button.clicked.connect(on_button_clicked)
+button.show()
 
 window = QWidget()
 print(window)
