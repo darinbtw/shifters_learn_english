a7d4c8d9e050cbe53e69928f115c55a596b6afda
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-06-25 09:02:02 +0300

    added new widget

--- app.py
+++ app.py
@@ -11,21 +11,42 @@ class MainWindow(QMainWindow):
         self.setWindowTitle('Учёба английскому!')
 
         layout = QVBoxLayout()
-
-        self.label = QLabel('Вам нужно писать перевод к этому слову')
+        self.label = QLabel('Вам нужно писать перевод к слову, которую вам предоставят!')
         layout.addWidget(self.label)
 
-        self.input_text = QLineEdit()
-        layout.addWidget(self.input_text)
+        self.label_start = QLabel('Начинаем?')
+        layout.addWidget(self.label_start)
 
-        self.check_button = QPushButton('Проверить перевод')
-        self.check_button.clicked.connect(self.check_translation)
+        self.check_button = QPushButton('Начать игру!')
+        self.check_button.clicked.connect(self.start_game)
         layout.addWidget(self.check_button)
 
         central_widget.setLayout(layout)
 
-    def check_translation(self):
-        print('Вы написали перевод!')
+    def start_game(self):
+        New_Widged = QWidget()
+        self.setCentralWidget(New_Widged)
+        self.setWindowTitle('Старт игры!')
+        
+        layout_start = QVBoxLayout()
+
+        self.Starts_words = QLabel('Тут должен быть текст заготовленный под какое-то слове')
+        layout_start.addWidget(self.Starts_words)
+        
+        self.input_text = QLineEdit()
+        
+        self.input_text.setPlaceholderText('Введите перевод текста:')
+        layout_start.addWidget(self.input_text)
+
+        self.
+
+        New_Widged.setLayout(layout_start)
+
+
+    def generated_english_words(self):
+         with open('words.txt', 'r', encoding='UTF-8') as file:
+              words = file.read()
+              print(words)
 
 def starts():
         app = QApplication(sys.argv)
