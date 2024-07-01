fcbbc0fa0d28a6f86dfe49e66d61e415d3b6d8ee
darinbtw <123627549+darinbtw@users.noreply.github.com>
2024-07-01 09:48:48 +0300

    added new function for requests dailog txt file

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QLineEdit, QMainWindow, QWidget, QVBoxLayout
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QPushButton, QVBoxLayout, QMessageBox, QMainWindow, QLineEdit
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QLineEdit, QMainWindow, QWidget, QVBoxLayout, QMessageBox, QFileDialog
 import sys
 from random import choice
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-    
         self.setWindowTitle('Учёба английскому языку!')
         self.main_menu()
 
@@ -14,16 +12,13 @@ class MainWindow(QMainWindow):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
 
-        #Текст с небольшим объяснением того, что нужно делать
         layout = QVBoxLayout()
         self.label = QLabel('Вам нужно писать перевод к слову, которую вам предоставят!')
         layout.addWidget(self.label)
 
-        #Создаётся другой текст с вопросом
         self.label_start = QLabel('Выберите режим:')
         layout.addWidget(self.label_start)
 
-        #Создаётся кнопка после которой нас перебрасывает на новое окно
         self.check_button = QPushButton('С Английского на Русский')
         self.check_button.clicked.connect(self.start_game)
         layout.addWidget(self.check_button)
@@ -31,20 +26,22 @@ class MainWindow(QMainWindow):
         self.russian_game = QPushButton('С Русского на Английский')
         self.russian_game.clicked.connect(self.russian_game_start)
         layout.addWidget(self.russian_game)
-        
+
         self.english_word_add = QPushButton('Добавить свои английски слова')
         self.english_word_add.clicked.connect(self.rule_forEnglish_words)
         layout.addWidget(self.english_word_add)
 
-        #Отображает все вставки
         central_widget.setLayout(layout)
 
     def russian_game_start(self):
-        self.new_widged = QWidget()
-        self.setCentralWidget(self.new_widged)
+        self.new_widget = QWidget()
+        self.setCentralWidget(self.new_widget)
         self.setWindowTitle('С Русского на Английский')
 
         self.layout_rus = QVBoxLayout()
+
+        self.starts_words_rus = QLabel()
+        self.layout_rus.addWidget(self.starts_words_rus)
 
         self.load_new_word_rus()
 
@@ -55,44 +52,64 @@ class MainWindow(QMainWindow):
         self.button_click_user = QPushButton('Отправить')
         self.button_click_user.clicked.connect(self.check_translate_rus)
         self.layout_rus.addWidget(self.button_click_user)
-        
+
         self.back_to_main_menu1 = QPushButton('Вернутся в меню')
         self.back_to_main_menu1.clicked.connect(self.main_menu)
         self.layout_rus.addWidget(self.back_to_main_menu1)
-        
-        self.russian_help = QPushButton('Показать овтет?')
+
+        self.russian_help = QPushButton('Показать ответ?')
         self.russian_help.clicked.connect(self.help_russian)
         self.layout_rus.addWidget(self.russian_help)
-        
-        self.new_widged.setLayout(self.layout_rus)
-        
+
+        self.new_widget.setLayout(self.layout_rus)
+
     def rule_forEnglish_words(self):
         self.new_widget3 = QWidget()
         self.setCentralWidget(self.new_widget3)
         self.setWindowTitle('Добавить новое английское слово')
-        
+
         self.layout2 = QVBoxLayout()
-        
+
         self.guide = QLabel('''Как же добавить новое английское слово? Вам нужно создать txt файл,
 в котором будет <Английское слово> <-> тире и <Перевод на русский это слово>
 Пример: apple - яблоко''')
         self.layout2.addWidget(self.guide)
-        
+
+        self.open_file = QPushButton('Добавить файл')
+        self.open_file.clicked.connect(self.open_file_dialog)
+        self.layout2.addWidget(self.open_file)
+
+        self.back_to_main_menu2 = QPushButton('Вернутся в главное меню')
+        self.back_to_main_menu2.clicked.connect(self.main_menu)
+        self.layout2.addWidget(self.back_to_main_menu2)
+
         self.new_widget3.setLayout(self.layout2)
-        
-    def add_new_word(self):
-        with open('english_words.txt', '', encoding='UTF=8')as file2:
-            line2 = file2.readline()
-            
-        
+    
+
+    def open_file_dialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+        file_name , _ = QFileDialog.getOpenFileName(self, 'Выберите файл','', 'Text Files (*.txt);;All Files(*)', options=options)
+        if file_name:
+            self.add_new_word(file_name)
+
+    def add_new_word(self, file_name):
+        try:
+            with open(file_name, 'r', encoding='UTF-8') as file:
+                lines = file.readline()
+                with open('english_words.txt', 'a', encoding='UTF-8') as english_word:
+                    english_word.writelines(lines)
+            QMessageBox.information(self, 'Успех', 'Слова успешны добавлены')
+        except Exception as e:
+            QMessageBox.warning(self, 'Ошибка', f'Не удалось добавить слова: {e}')
+
     def help_russian(self):
         if hasattr(self, 'help_russian1'):
-            self.help_russian1.setText(f'Показать ответ {self.correct_translition_rus}')
-            self.show()
+            self.help_russian1.setText(f'Ответ: {self.correct_translition_rus}')
         else:
-            self.help_russian1 = QLabel(f'Показать ответ {self.correct_translition_rus}')
+            self.help_russian1 = QLabel(f'Ответ: {self.correct_translition_rus}')
             self.layout_rus.addWidget(self.help_russian1)
-            
+
     def check_translate_rus(self):
         user_input_text = self.input_text_user.text()
 
@@ -103,28 +120,25 @@ class MainWindow(QMainWindow):
             QMessageBox.warning(self, 'Результат', 'Вы не правильно перевели!')
 
     def start_game(self):
-        #Создаём новое окно
-        self.new_Widged = QWidget()
-        self.setCentralWidget(self.new_Widged)
+        self.new_widget = QWidget()
+        self.setCentralWidget(self.new_widget)
         self.setWindowTitle('С Английского на Русский')
-        
-        #Будет добавлять вставки, текст и т.д
+
         self.layout_start = QVBoxLayout()
 
-        #Выводим слово которое нужно перевести
+        self.starts_words = QLabel()
+        self.layout_start.addWidget(self.starts_words)
+
         self.load_new_words()
-        
-        #Создаётся строчка для ввода пользователя
+
         self.input_text = QLineEdit()
         self.input_text.setPlaceholderText('Введите перевод слова')
         self.layout_start.addWidget(self.input_text)
 
-        #Кнопка после которой идёт проверка на правильность перевода
         self.button_check = QPushButton('Проверить перевод')
         self.button_check.clicked.connect(self.check_translate)
         self.layout_start.addWidget(self.button_check)
 
-        #Кнопка которая возвращает нас в главное меню
         self.back_main_menu = QPushButton('Вернуться обратно в меню')
         self.back_main_menu.clicked.connect(self.main_menu)
         self.layout_start.addWidget(self.back_main_menu)
@@ -132,65 +146,53 @@ class MainWindow(QMainWindow):
         self.helping = QPushButton('Показать ответ?')
         self.helping.clicked.connect(self.helps)
         self.layout_start.addWidget(self.helping)
-        
-        #Отображает все вставки
-        self.new_Widged.setLayout(self.layout_start)
 
-    #Функция которая проверяет ввёдное слово пользователем на правильность перевода
+        self.new_widget.setLayout(self.layout_start)
+
     def check_translate(self):
         user_input = self.input_text.text()
         if user_input.lower() == self.correct_translition.lower():
-             QMessageBox.information(self, 'Результат', 'Вы правильно перевели')
-             self.load_new_words()
+            QMessageBox.information(self, 'Результат', 'Вы правильно перевели')
+            self.load_new_words()
         else:
-             QMessageBox.warning(self, 'Результат', 'Вы неправильно перевели, попробуйте ещё раз!')
+            QMessageBox.warning(self, 'Результат', 'Вы неправильно перевели, попробуйте ещё раз!')
 
     def load_new_words(self):
         self.english_word, self.correct_translition = self.generated_english_words()
-        if hasattr(self, 'starts_words'):
-            self.starts_words.setText(f'Переведите слово: {self.english_word}')
-        else:
-            self.starts_words = QLabel(f'Переведите слово: {self.english_word}')
-            self.layout_start.addWidget(self.starts_words)
+        self.starts_words.setText(f'Переведите слово: {self.english_word}')
 
-    #Генерирует слово из words.txt
     def generated_english_words(self):
-         with open('english_words.txt', 'r', encoding='UTF-8') as file:
-              lines = file.readlines()
-              random_line = choice(lines).strip()
-              english_word, russian_word = random_line.split(' - ')
-              return english_word, russian_word
-    
+        with open('english_words.txt', 'r', encoding='UTF-8') as file:
+            lines = file.readlines()
+            random_line = choice(lines).strip()
+            english_word, russian_word = random_line.split(' - ')
+            return english_word, russian_word
+
     def load_new_word_rus(self):
         self.russian_word, self.correct_translition_rus = self.generated_russian_words()
-        if hasattr(self, 'starts_words_rus'):
-            self.starts_words_rus.setText(f'Переведите слово: {self.russian_word}')
-        else:
-            self.starts_words_rus = QLabel(f'Переведите слово: {self.russian_word}')
-            self.layout_rus.addWidget(self.starts_words_rus)
-            
+        self.starts_words_rus.setText(f'Переведите слово: {self.russian_word}')
+
     def generated_russian_words(self):
         with open('russian_words.txt', 'r', encoding='UTF-8') as file1:
-            line1 = file1.readlines()
-            random_line1 = choice(line1).strip()
+            lines = file1.readlines()
+            random_line1 = choice(lines).strip()
             russian_words1, english_words1 = random_line1.split(' - ')
-            return russian_words1,english_words1
-    
+            return russian_words1, english_words1
+
     def helps(self):
         if hasattr(self, 'help'):
-            self.help.setText(f'Правильный перевод: {self.correct_translition}')
-            self.help.show()
+            self.help.setText(f'Ответ: {self.correct_translition}')
         else:
-            self.help = QLabel(f'Правильный перевод: {self.correct_translition}')
+            self.help = QLabel(f'Ответ: {self.correct_translition}')
             self.layout_start.addWidget(self.help)
 
-#Запуск всего проекта
+# Запуск всего проекта
 def starts():
-        app = QApplication(sys.argv)
-        app.setStyle('Fussion') #WindowsXP, Windows, Fussion, WindowsVista
-        window = MainWindow()   
-        window.show()
-        sys.exit(app.exec_())
+    app = QApplication(sys.argv)
+    app.setStyle('Fusion')
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
     starts()
